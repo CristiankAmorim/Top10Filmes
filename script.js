dd8d3c70a7dee09a52c3aee3fd9afabe75a3db35
Cristian Amorim da Silva <klaywert11@gmail.com>
2025-05-26 10:55:28 -0300

    Enviando arquivos css e js

---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleClass(element) {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        if(card !== element) {
+            card.classList.remove('card-filme-select');
+            card.classList.add('card-filme');
+
+            const conteudo = card.querySelector('.conteudo');
+            if(conteudo) {
+                conteudo.classList.remove('visivel');
+                conteudo.classList.add('escondido');
+            }
+        }
+    });
+
+    element.classList.toggle('card-filme');
+    element.classList.toggle('card-filme-select');
+
+    const conteudoInterno = element.querySelector('.conteudo');
+    if(conteudoInterno) {
+        const isVisivel = conteudoInterno.classList.contains('visivel');
+
+        if(isVisivel) {
+            conteudoInterno.classList.remove('visivel');
+            conteudoInterno.classList.add('escondido');
+        } else {
+            conteudoInterno.classList.remove('escondido');
+            conteudoInterno.classList.add('visivel');
+        }
+    }
+}
